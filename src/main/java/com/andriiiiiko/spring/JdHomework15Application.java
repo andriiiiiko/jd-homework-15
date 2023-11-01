ed64e58f864481ea6c094be6aa7025ffdc83e697
Andrii Protas <andreyprotas5@gmail.com>
2023-11-01 12:26:28 +0100

    Initial commit

---
+++ src/main/java/com/andriiiiiko/spring/JdHomework15Application.java
@@ -0,0 +1,13 @@
+package com.andriiiiiko.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdHomework15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdHomework15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andriiiiiko/spring/JdHomework15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriiiiiko.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdHomework15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
